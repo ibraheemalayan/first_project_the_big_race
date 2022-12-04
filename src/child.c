0f2a26a734fd89f562ee1c270c8b09de8d52e85b
Ibraheem Alyan <ibraheemalayan@gmail.com>
2022-12-04 14:15:09 +0200

    👨🏻‍💻 Basic initial backend code

--- src/child.c
+++ src/child.c
@@ -4,37 +4,61 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 #include <time.h>
 #include <errno.h>
 
 #include "./std.h"
 
 pid_t next_player_pid;
-
+int team_num;
+int team_signal;
 int speed;
-int distance = 100;
-// #define foo4random() (arc4random() % ((unsigned)RAND_MAX + 1))
+int distance = 25;
+
+typedef void (*sighandler_t)(int);
+
+sighandler_t sigset(int sig, sighandler_t disp);
+
+#define init_random() (srand(time(NULL) ^ (getpid() << 16)))
+
+void send_signal_to_next_player();
+void start_signal_catcher(int the_sig);
 
 void validate_args(int argc, char *argv[])
 {
 
-    if (argc < 2)
+    if (argc < 3)
     {
         errno = EINVAL;
         red_stderr();
-        perror("\nMissing the pid of next child argument");
+        perror("\nMust pass two arguments: pid of next player, team number");
         reset_stderr();
         exit(-1);
     }
 
+    // the first argument is -1 if last child
     if (!(next_player_pid = atoi(argv[1])))
     {
         errno = EINVAL;
-        red_stderr();
-        perror("\nThe first argument must be the pid of next player");
-        reset_stderr();
+        red_stdout();
+        printf("\nThe first argument must be the pid of next player, instead of %s", argv[1]);
+        fflush(stdout);
+        reset_stdout();
         exit(-1);
     };
+
+    if (!(team_num = atoi(argv[2])) || (team_num != 1 && team_num != 2))
+    {
+        errno = EINVAL;
+        red_stdout();
+        printf("\nThe second argument must be the team number which can be only 1 or 2 instead of %s", argv[2]);
+        fflush(stdout);
+        reset_stdout();
+        exit(-1);
+    };
+
+    team_signal = (team_num == 1) ? SIGUSR1 : SIGUSR2;
 }
 
 int main(int argc, char *argv[])
@@ -42,15 +66,53 @@ int main(int argc, char *argv[])
 
     validate_args(argc, argv);
 
-    srand(time(NULL) ^ (getpid() << 16));
+    init_random();
 
-    speed = rand() % 10 + 5;
+    speed = rand() % 15 + 5;
 
-    magenta_stdout();
+    if (sigset(team_signal, start_signal_catcher) == -1) // FIXME keeps emitting a warning
+    {
+        perror("Sigset can not set team signal");
+        exit(SIGQUIT);
+    }
 
-    printf("\nChild Process Generated Speed (%d) and has argument %d and pid : %d and parent id : %d\n", speed, next_player_pid, getpid(), getppid());
+    printf("\n%d: Child generated with speed=%d, next_player_pid=%d, team=%d", getpid(), speed, next_player_pid, team_num);
+    fflush(stdout);
 
-    reset_stdout();
+    while (1)
+    {
+        pause(); // wait for a signal
+    }
 
     return (0);
+}
+
+void start_signal_catcher(int the_sig)
+{
+
+    // SIGUSR1 will be used for start
+    // SIGUSR2 will be used for reset
+
+    printf("\nStart signal for pid %d in team %d was received.\n", getpid(), team_num);
+
+    if (the_sig == team_signal)
+    {
+        sleep(distance / speed);
+        send_signal_to_next_player();
+    }
+
+    exit(1);
+}
+
+void send_signal_to_next_player()
+{
+
+    if (next_player_pid != -1)
+    {
+        kill(next_player_pid, team_signal); // send team signal to next player
+    }
+    else
+    {
+        kill(getppid(), team_signal); // send team signal to parent
+    }
 }
--- src/parent.c
+++ src/parent.c
@@ -3,14 +3,26 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
 #include "./std.h"
 
+#define PLAYERS_PER_TEAM 5
+
 int rounds;
 int current_round = 0;
+
+int current_round_result = 0;
+
+int team_1_score = 0, team_2_score = 0;
+
+void signal_catcher(int the_sig);
+
+// green team from 0-4 , red team 5-9
+pid_t players_pids[PLAYERS_PER_TEAM * 2];
 
 void validate_args(int argc, char *argv[])
 {
@@ -41,47 +53,157 @@ int main(int argc, char *argv[])
 
     validate_args(argc, argv);
 
-    for (current_round = 0; current_round < rounds; current_round++)
+    int status, i = PLAYERS_PER_TEAM * 2 - 1;
+
+    green_stdout();
+    printf("\n\n\n\nParent process ID is %d\n", getpid());
+
+    // set to zero for the last child [means send signal to parent on finish]
+    char next_player_pid_arg[8];
+    sprintf(next_player_pid_arg, "-1");
+
+    char team_arg[3];
+    sprintf(team_arg, "2");
+
+    magenta_stdout();
+    fflush(stdout);
+
+    for (; i > -1; i--)
     {
-        green_stdout();
-        printf("\n\n> Round #%d out of #%d ...\n\n", current_round + 1, rounds);
-        reset_stdout();
-    }
+        pid_t current_fork_pid;
 
-    int status, i = 0, num_of_forks = rounds;
-
-    pid_t children_pids[num_of_forks];
-
-    printf("\n\nParent process ID is %d\n", getpid());
-
-    for (; i < num_of_forks; i++)
-    {
-
-        pid_t current;
-
-        if ((current = fork()) == -1)
+        if ((current_fork_pid = fork()) == -1)
         {
             perror("fork failure ... getting out\n");
+            // TODO kill other children
             exit(-1);
         }
 
-        if (current == 0)
+        if (current_fork_pid == 0)
         {
-            // child
-            execlp("./bin/child", "child", "4", (char *)NULL);
-            exit(-1);
+            // execute child with next player pid as argument
+            execlp("./bin/child", "child", next_player_pid_arg, team_arg, (char *)NULL);
+            perror("exec failure ");
+            exit(-2);
         }
         else
         {
             // parent
-            children_pids[i] = current;
+            players_pids[i] = current_fork_pid;
+            if (i == 5)
+            {
+                printf("Switching to team 1");
+                sprintf(next_player_pid_arg, "-1");
+                sprintf(team_arg, "1");
+            }
+            else
+            {
+                sprintf(next_player_pid_arg, "%d", current_fork_pid);
+            }
         }
+
+        // breakpoint here
     }
 
-    for (i = 0; i < num_of_forks; i++)
+    reset_stdout();
+    fflush(stdout);
+
+    if (sigset(SIGUSR1, signal_catcher) == -1) // FIXME keeps emitting a warning
     {
-        wait(&status);
+        perror("Sigset can not set SIGUSR1 catcher");
+        exit(SIGQUIT);
+    }
+
+    if (sigset(SIGUSR2, signal_catcher) == -1) // FIXME keeps emitting a warning
+    {
+        perror("Sigset can not set SIGUSR2 catcher");
+        exit(SIGQUIT);
+    }
+
+    for (current_round = 0; current_round < rounds;)
+    {
+        green_stdout();
+        run_round(current_round);
+    }
+
+    for (i = 0; i < PLAYERS_PER_TEAM * 2; i++)
+    {
+        pid_t c_id = wait(&status);
+        printf("\nchild with id %d has exited with status %d\n", c_id, status);
+        fflush(stdout);
+        sleep(1);
     }
 
     return (0);
+}
+
+void signal_catcher(int the_sig)
+{
+
+    // SIGUSR1 will be used by team 1
+    // SIGUSR2 will be used by team 2
+
+    if (current_round_result == 1)
+    {
+        current_round_result++;
+        if (the_sig == SIGUSR1)
+        {
+            printf("\n\n!!! Team 1 has lost this round !!!\n\n");
+            current_round_result = 2;
+            current_round++;
+            return;
+        }
+        else if (the_sig == SIGUSR2)
+        {
+            printf("\n\n!!! Team 2 has lost this round !!!\n\n");
+            current_round_result = 2;
+            current_round++;
+            return;
+        }
+        exit(-1);
+    }
+
+    if (the_sig == SIGUSR1)
+    {
+        team_1_score++;
+        printf("\n\n!!! Team 1 has won this round !!!\n\n");
+        current_round_result = 1;
+        return;
+    }
+    else if (the_sig == SIGUSR2)
+    {
+        team_2_score++;
+        printf("\n\n!!! Team 2 has won this round !!!\n\n");
+        current_round_result = 1;
+        return;
+    }
+
+    exit(1);
+}
+
+void run_round(running_round)
+{
+
+    // SIGCLD is used to start next round
+
+    green_stdout();
+    printf("\n\n> Round #%d out of #%d will start in 3 second ...\n\n", current_round + 1, rounds);
+    reset_stdout();
+
+    sleep(3);
+
+    current_round_result = 0;
+
+    kill(players_pids[0], SIGUSR1);
+    kill(players_pids[5], SIGUSR2);
+
+    printf("\n\nWaiting for round #%d to finish ...\n\n", current_round + 1);
+
+    // wait for current round to finish ( finishes when both teams have finished )
+    while (running_round == current_round)
+    {
+        pause(); // this will return after a signal handler returns
+    }
+
+    printf("\n\nRound #%d finished and current scores are\n\tteam green - %d\n\t team red - %d\n\n", current_round + 1, team_1_score, team_2_score);
 }
